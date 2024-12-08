2794b52666ece1397f40f962d1d252e25d32abbd
Liam Murray <122718074+lmurray045@users.noreply.github.com>
2024-12-08 09:25:02 -0800

    Changed vite config for pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+//import { defineConfig } from "vite";
 
-export default defineConfig({
+export default {
+  base: process.env.REPO_NAME || "/repo-name/",
   resolve: {
     extensions: [".ts", ".js"], // Ensure TypeScript and JavaScript files are resolved
   },
-});
+}
